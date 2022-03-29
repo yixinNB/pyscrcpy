2aff40e090431fca0577fa56095dad69bb8ac1a4
S1M0N38 <bertolottosimone@gmail.com>
2022-03-29 11:37:49 +0200

    first test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,21 @@
 from scrcpy import Client
+import time
 
+def make_on_frame():
+    count = 0
 
-def resolution(client, frame):
-    print(frame.shape)
+    def on_frame(client, frame):
+        nonlocal count
+        #if count % 30 == 0:
+        print(f"{count} {frame.shape}")
+        count += 1
+
+    return on_frame
+
 
 
 def test_client():
-    client = Client(block_frame=True)
-    client.on_frame(resolution)
+    client = Client(block_frame=True, stay_awake=True, max_fps=1)
+    client.on_frame(make_on_frame())
     client.start()
+
